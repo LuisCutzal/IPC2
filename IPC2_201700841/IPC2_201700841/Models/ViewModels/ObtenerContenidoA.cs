fbe5d926400aa86c45bb56a588308b3120423167
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-10-01 21:17:30 -0600

    primero
    primero

---
+++ IPC2_201700841/IPC2_201700841/Models/ViewModels/ObtenerContenidoA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPC2_201700841.Models.ViewModels
+{
+    public class ObtenerContenidoA
+    {
+        public int id { get; set; }
+        public string color { get; set; }
+        public string columna { get; set; }
+        public Nullable<int> fila { get; set; }
+
+       
+        //prop coloca de una vez public string nombre {get;set;}
+    }
+}
